826e3232aac6641df4bf5b85cd4e1bf6ca5b02a0
Ilyasse Aldidi <i.aldidi@alphalink.fr>
2026-05-25 12:02:52 +0100

    Change app name from inception to app

--- app/services/nginx/tools/script.sh
+++ app/services/nginx/tools/script.sh
@@ -5,6 +5,6 @@ mkdir -p /etc/nginx/ssl
 openssl req -x509 -noenc \
     -keyout /etc/nginx/ssl/nginx.key \
     -out /etc/nginx/ssl/nginx.crt \
-    -subj "/CN=${DOMAIN_NAME:-localhost}"
+    -subj "/CN=ialdidi.42.fr"
 
 exec nginx -g "daemon off;"
